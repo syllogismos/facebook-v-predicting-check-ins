680a90c3f847a64d9ce2df767ca2b3a80ab5d98f
syllogismos <anilkaraka@live.in>
2016-06-19 20:39:38 +0530

    refactor

--- scripts/base_scikit_learn_model.py
+++ scripts/base_scikit_learn_model.py
@@ -56,9 +56,23 @@ class SklearnModel(BaseModel):
         if y_transformer == None:
             label_encoder = LabelEncoder()
             label_encoder.fit(y)
-            y_transformer = {'label_encoder': label_encoder}
-        new_y = y_transformer['label_encoder'].transform(y).reshape(-1, 1)
+            y_transformer = {'encoder': label_encoder}
+        new_y = y_transformer['encoder'].transform(y).reshape(-1, 1)
         return (new_y, y_transformer)
+
+    def custom_classifier(self, X, Y):
+        """
+        Given X, Y
+        define the model, and train it
+        """
+        if self.grid.pref == 'test':
+            clf = KNeighborsClassifier(n_neighbors = 2,\
+                weights = 'distance', metric = 'manhattan')
+        else:
+            clf = KNeighborsClassifier(n_neighbors = 29,\
+                weights = 'distance', metric = 'manhattan')
+        clf.fit(X, np.ravel(Y))
+        return clf
 
     def train(self, force = False):
         """
@@ -86,6 +100,7 @@ class SklearnModel(BaseModel):
 
     def train_grid(self, m, n):
         """
+        Helper function for train function that takes the grid cell to be trained
         """
         print "Training %s, %s grid" %(m, n)
         init_time = time.time()
@@ -106,13 +121,7 @@ class SklearnModel(BaseModel):
             # if masked data is of length zero then also make model None
             self.model[m][n]['model'] = None
         else:
-            if self.grid.pref == 'test':
-                self.model[m][n]['model'] = KNeighborsClassifier(n_neighbors = 2,\
-                    weights = 'distance', metric = 'manhattan')
-            else:
-                self.model[m][n]['model'] = KNeighborsClassifier(n_neighbors = 29,\
-                    weights = 'distance', metric = 'manhattan')
-            self.model[m][n]['model'].fit(X, np.ravel(Y))
+            self.model[m][n]['model'] = self.custom_classifier(X, Y)
 
         print "Time taken to train grid %s, %s is: %s" %(m, n, time.time() - init_time)
         print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
@@ -153,7 +162,7 @@ class SklearnModel(BaseModel):
             top_3_placeids = np.array([[5348440074, 9988088517, 4048573921]]*len(temp_x))
         else:
             prediction_probs = self.model[m][n]['model'].predict_proba(temp_x)
-            top_3_placeids = self.model[m][n]['y_transformer']['label_encoder'].inverse_transform(np.argsort(prediction_probs, axis=1)[:,::-1][:,:3])
+            top_3_placeids = self.model[m][n]['y_transformer']['encoder'].inverse_transform(np.argsort(prediction_probs, axis=1)[:,::-1][:,:3])
 
             # temporary hack when the no of predictions for a row is less than 3
             x, y = top_3_placeids.shape
@@ -164,11 +173,15 @@ class SklearnModel(BaseModel):
         return np.hstack((grid_data[:, 0].reshape(-1, 1), top_3_placeids))
 
     def generate_submission_file(self, submission_file):
+        """
+        Generate the submission file using the trained model in each indivudual grid
+        """
         test_data = np.loadtxt(self.test_file, dtype = float, delimiter = ',')
         predictions = self.predict(test_data)
         predictions = predictions.astype(int)
         predictions = predictions.astype(str)
         submission = open(submission_file, 'wb')
+        submission.write('row_id,place_id\n')
         for i in range(len(predictions)):
             row = predictions[i]
             row_id = row[0]
@@ -179,6 +192,9 @@ class SklearnModel(BaseModel):
         submission.close()
 
     def check_cross_validation(self):
+        """
+        Compute cross validation of the trained model on the cv file that we initialized
+        """
         data = np.loadtxt(self.cross_validation_file, dtype = float, delimiter = ',')
         predictions = self.predict(data)
         predictions = predictions.astype(int)
@@ -186,3 +202,4 @@ class SklearnModel(BaseModel):
         preds = np.hstack((predictions, actual))
         apk_list = map(lambda row: apk(row[-1:], row[1: -1]), preds)
         self.cv_mean_precision = np.mean(apk_list)
+        return self.cv_mean_precision
