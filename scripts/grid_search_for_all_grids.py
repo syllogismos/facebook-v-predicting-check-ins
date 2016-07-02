4f448a9f1dd3a56683c6a0f3b87acd6525bdabbc
syllogismos <anilkaraka@live.in>
2016-07-02 12:25:23 +0530

    fix bugs

--- scripts/grid_search_for_all_grids.py
+++ scripts/grid_search_for_all_grids.py
@@ -22,7 +22,7 @@ import xgboost as xgb
 import traceback
 import grid_generation as grid
 
-
+from helpers import zip_file_and_upload_to_s3
 
 params_range = {'a': range(1, 5),
           'b': range(6, 9)}
@@ -135,8 +135,8 @@ YY = 10000
 
 eps = 0.001
 
-m = XX/xd
-n = YY/yd
+m = XX/X
+n = YY/Y
 
 M = XX/xD
 N = YY/yD
@@ -155,19 +155,21 @@ def get_list_of_params(params_range):
 
 def grid_search_xgb(params_range_dict):
     grid_params_list = get_list_of_params(params_range_dict)
-    p = Pool(4)
+    p = Pool(8)
     maps = p.map(get_map_of_xgb, grid_params_list)
+    p.close()
+    p.join()
     sorted_maps = sorted(maps, cmp = lambda x, y: cmp(x['map'], y['map']), reverse = True)
     print "top map results", sorted_maps[:3]
     return sorted_maps
 
 def get_map_of_xgb(grid_param):
-    global orig_params
+    cv_params = dict(orig_params)
     num_class = {'num_class': len(enc['encoder'].classes_)}
-    orig_params.update(num_class)
-    orig_params.update(grid_param)
+    cv_params.update(num_class)
+    cv_params.update(grid_param)
     # print orig_params, grid_param
-    temp_cv = xgb.cv(orig_params, dtrain, num_boost_round = 10,
+    temp_cv = xgb.cv(cv_params, dtrain, num_boost_round = 100,
              early_stopping_rounds = 20, feval = map3eval, maximize = True)
     temp_map = temp_cv['test-MAP@3-mean'][temp_cv.shape[0]-1]
     grid_param['map'] = temp_map
@@ -177,7 +179,7 @@ def get_map_of_xgb(grid_param):
 #m, n = (12, 50) (37, 50) (12, 150) (37, 150)
 #dtrain, enc = get_dtrain_enc(12, 50)
 param_range1 = {
-    'max_depth': range(2, 7, 1),
+    'max_depth': range(2, 9, 1),
     'min_child_weight': range(1, 7, 1)
 }
 
@@ -197,57 +199,58 @@ param_range4 = {
 
 
 
+if __name__ == '__main__':
 
-params_dict = [[{} for nt in range(n + 1)] for mt in range(m + 1)]
-param_range1 = {
-    'max_depth': range(2, 3, 1),
-    'min_child_weight': range(1, 2, 1)
-}
+    params_dict = [[{} for nt in range(g.max_n + 1)] for mt in range(g.max_m + 1)]
 
-for Mt in range(M):
-    for Nt in range(N):
-        orig_params = {
-            'silent': 0,
-            'nthread': 8,
-            'eta': 0.1,
-            'objective': 'multi:softprob',
-            'max_depth': 8,
-            'min_child_weight': 5,
-            'gamma': 0.32,
-            'subsample': 0.9,
-            'colsample_bytree': 0.7,
-            'scale_pos_weight': 1
-            }
-        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-        print Mt, "Mt"
-        print Nt, "Nt"
-        x = (Mt * xD)*eps + eps
-        y = (Nt * yD)*eps + eps
-        print x, y
-        (mt, nt) = grid.get_grids((x, y), X, Y, xd, yd)[0]
-        print mt, "mt"
-        print nt, "nt"
+    for Mt in range(M):
+        for Nt in range(N):
+            orig_params = {
+                'silent': 0,
+                'nthread': 8,
+                'eta': 0.1,
+                'objective': 'multi:softprob',
+                'max_depth': 8,
+                'min_child_weight': 5,
+                'gamma': 0.32,
+                'subsample': 0.9,
+                'colsample_bytree': 0.7,
+                'scale_pos_weight': 1
+                }
+            print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            print Mt, "Mt"
+            print Nt, "Nt"
+            x = (Mt * xD)*eps + eps
+            y = (Nt * yD)*eps + eps
+            print x, y
+            (mt, nt) = grid.get_grids((x, y), X, Y, xd, yd)[0]
+            print mt, "mt"
+            print nt, "nt"
 
-        dtrain, enc = get_dtrain_enc(mt, nt)
+            dtrain, enc = get_dtrain_enc(mt, nt)
 
-        for param_range in [param_range1]:#, param_range2, param_range3, param_range4]:
-            result = None
-            try:
-                result = grid_search_xgb(param_range)
-            except Exception, e:
-                print e
-                print traceback.format_exc()
+            for param_range in [param_range1]:#, param_range2, param_range3, param_range4]:
+                result = None
+                try:
+                    result = grid_search_xgb(param_range)
+                except Exception, e:
+                    print e
+                    print traceback.format_exc()
 
-            if result != None:
-                temp_param = result[0]
-                del(temp_param['map'])
-                orig_params.update(temp_param)
-        for mc in range(rx + 1):
-            for nc in range(ry + 1):
-                if (mt + mc) < (g.max_m + 1) and (nt + nc) < (g.max_n + 1):
-                    params_dict[mt + mc][nt + nc] = dict(orig_params) 
+                if result != None:
+                    temp_param = result[0]
+                    del(temp_param['map'])
+                    orig_params.update(temp_param)
+            for mc in range(rx + 1):
+                for nc in range(ry + 1):
+                    if (mt + mc) < (g.max_m + 1) and (nt + nc) < (g.max_n + 1):
+                        params_dict[mt + mc][nt + nc] = dict(orig_params)
 
-        print "computed params for big grid %s, %s" %(Mt, Nt)
-        print orig_params
-        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            print "computed params for big grid %s, %s" %(Mt, Nt)
+            print orig_params
+            print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+    fName = '../grid_200_50_20_5_rx5_ry10_params_dict.pickle'
+    pickle.dump(params_dict, open(fName, 'wb'))
+    zip_file_and_upload_to_s3(fName)
 
